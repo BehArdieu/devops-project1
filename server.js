96b5a64d82d420d603ab9118092d581c86871cb6
Comlan ALOTIN <alotinbehardieu@gmail.com>
2024-08-24 02:21:26 +0200

    Test 2 pipeline

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, DevOps!');
+  res.send('Hello, DevOps World!');
 });
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+    console.log(`My Node App listening at http://localhost:${port}`);
   });
 }
 
